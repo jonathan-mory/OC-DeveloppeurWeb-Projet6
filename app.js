d10484b2e6d8552fe48b3bb79593bab77a182d65
Jonathan Mory <jomory@orange.fr>
2025-02-11 14:23:34 +0100

    Installation de Mongoose et paramétrage de la BDD MongoDB

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
-const express = require("express")
+const express = require('express');
+const mongoose = require('mongoose');
 
-const app = express()
+const bookRoutes = require('./routes/books');
 
-app.use((req, res) => {
-    res.json({ message: "Votre requête a bien été reçue !" })
-})
+const app = express();
+mongoose
+    .connect(
+        'mongodb+srv://jomory:UhQvdnz7M8O1R9LA@openclassrooms.elow0.mongodb.net/?retryWrites=true&w=majority&appName=OpenClassrooms'
+    )
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-module.exports = app
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+    );
+    next();
+});
+
+app.use('/api/books', bookRoutes);
+
+module.exports = app;
